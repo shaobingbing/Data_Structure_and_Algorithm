2ff6e8c4f159ecaceb9f985964df87607ae8d8ac
ShaoBingbing <1144250366@qq.com>
2019-07-26 22:54:18 +0800

    ArrayList

---
+++ src/com/shbing/util/list/MyArrayList.java
@@ -0,0 +1,10 @@
+package com.shbing.util.list;
+
+public class MyArrayList<AnyType> implements Iterable<AnyType> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int theSize;
+    private AnyType [] theItems;
+
+    public java.util.Iterator<AnyType> iterator() {
+    }
+}
